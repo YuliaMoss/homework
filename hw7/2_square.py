327af3ef9f0b247fd35ec3a4913eb3979749683d
BOPOH <mospan.yulia@gmail.com>
2023-09-20 11:23:37 +0300

    fix of second and third task

--- hw7/2_square.py
+++ hw7/2_square.py
@@ -1,11 +1,13 @@
 """2- Напишіть функцію square, яка приймає 1 аргумент, сторону квадрата, і повертає 3 значення :
 периметр квадрата, площа квадрата та діагональ квадрата. Надрукуйте їх"""
 
+import math
 
-def square(num):
-    perimeter_quad = num + num
-    square_quad = num * num
-    diagonal_quad = square_quad * square_quad
+
+def square(side_length):
+    perimeter_quad = side_length + side_length
+    square_quad = side_length * side_length
+    diagonal_quad = side_length * math.sqrt(2)
     return perimeter_quad, square_quad, diagonal_quad
 
 
--- hw7/3_display_box.py
+++ hw7/3_display_box.py
@@ -7,21 +7,19 @@ x   x
 xxxxx"""
 
 
-def display_box(width: int, high: int, character="*"):
+def display_box(width: int, height: int, character="*"):
     result = []
     width_print = width * character
     result.append(width_print)
 
+    height_print = character + ' ' * (width - 2)
     if width >= 2:
-        high_print = character + ' ' * (width - 2) + character
-        for h in range(high - 2):
-            result.append(high_print)
-    else:
-        high_print = character * (high - 2)
-        for h in high_print:
-            result.append(h)
+        height_print = height_print + character
 
-    if high >= 2:
+    for i in range(height - 2):
+        result.append(height_print)
+
+    if height >= 2:
         result.append(width_print)
 
     return "\n".join(result)
@@ -29,7 +27,9 @@ def display_box(width: int, high: int, character="*"):
 
 if __name__ == '__main__':
     print(display_box(5, 4, "x"))
+    print(display_box(6, 10, "x"))
     print(display_box(1, 1, "x"))
     print(display_box(2, 2, "x"))
-    print(display_box(4, 1, "x"))
     print(display_box(1, 4, "x"))
+
+
